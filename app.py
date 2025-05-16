b60392b9fb21083110757e618945336bd1872221
dawson-am01 <dawson.am01@gmail.com>
2025-05-16 14:20:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,11 +48,11 @@ st.header("⚖️ Entity Weightings")
 default_weights = {
     "Brand": 1,
     "Sport": 1,
-    "Competition": 1,
+    "Competition": 5,
     "Grade": 3,
-    "Market": 4,
-    "TimeBased": 5,
-    "Cohort": 6
+    "Market": 7,
+    "TimeBased": 15,
+    "Cohort": 30
 }
 
 entity_weights = {}
